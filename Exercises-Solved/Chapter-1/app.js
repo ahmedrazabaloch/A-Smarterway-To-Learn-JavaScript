e339bb12ca47e74eb273ab6fd8e525dad9b6e657
ahmedraza <ahmed.raza1054@gmail.com>
2024-03-10 22:14:46 +0500

    added chapter one solve

---
+++ Exercises-Solved/Chapter-1/app.js
@@ -0,0 +1,25 @@
+// ==================== Assignment # 1 ===================
+
+
+
+// 1. Write a script to greet your website visitor using JS alert box.
+
+alert("Welcome")
+
+// 2. Write a script to display following message on your web page:
+
+alert("Error! Please enter a valid password")
+
+// 3. Write a script to display following message on your web page: (Hint : Use line break)
+
+alert("Welcome to JS Land... \nHappy Coding!")
+
+// 4. Write a script to display following messages in sequence:
+
+alert("Welcome to JS Land...")
+alert("Happy Coding!")
+
+// 5. Generate the following message through browser’s developer console:
+
+console.log("Hello... I can run JS through my web browser's console")
+
