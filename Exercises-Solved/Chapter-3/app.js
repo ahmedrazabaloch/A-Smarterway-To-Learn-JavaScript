761de4218a212da5197354059f0975daacfb94d2
ahmedraza <ahmed.raza1054@gmail.com>
2024-03-12 00:50:37 +0500

    chapter 3 solve added

---
+++ Exercises-Solved/Chapter-3/app.js
@@ -0,0 +1,28 @@
+// ============== Assignment # 3 ================
+
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+
+// var age = 20
+
+// alert("I am " + age + " years old")
+
+// 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You have visited this site N times”.
+
+// 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+
+// var birthYear = 2000;
+
+// document.write("My birth year is " + birthYear + "<br/> Data type of my declared variable is " + typeof birthYear)
+
+// 4. A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to order Show the following message in your browser: 
+// “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”
+
+// var visitorName = prompt("Please enter your name", "John Doe")
+// var productTitle = prompt("Product Title", "5 T-shirt(s)")
+
+// document.write("<b>" + visitorName + "</b>" + " Ordered " + "<b>" + productTitle + "</b>" + " on XYZ Cloting store.")
+
